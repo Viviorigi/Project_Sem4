9cad212b187a5f9ef9e497c6e550a670398c7b43
Viviorigi <duong682@gmail.com>
2025-10-14 16:35:08 +0700

    .

--- sem3_csharp-master/AspnetApi/obj/Debug/net8.0/AspnetApi.AssemblyInfo.cs
+++ sem3_csharp-master/AspnetApi/obj/Debug/net8.0/AspnetApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspnetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d942dcf600875dbaf62c6cc78e6abb043d4dc4e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5771350b522997112d59a87a94b58d403058daf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspnetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspnetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
